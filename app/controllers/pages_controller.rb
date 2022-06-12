e4b649edfd784164d6aedd9362f98d63c9cc18e8
David O'Regan <doregan@gitlab.com>
2022-06-12 19:02:13 +0100

    feat: add create action for wiki pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,23 @@ class PagesController < ApplicationController
   def show
     @page = Page.find(params[:id])
   end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to @page
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:title, :body)
+    end
 end
